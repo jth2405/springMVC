492d672ae85fca4376bb9b1592909a73731e0b4b
jth0880 <jth0880@gmail.com>
2022-02-15 20:48:23 +0900

    39강

--- src/main/java/webprj/controller/customer/NoticeController.java
+++ src/main/java/webprj/controller/customer/NoticeController.java
@@ -3,9 +3,12 @@ package webprj.controller.customer;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import webprj.entity.Notice;
 import webprj.service.NoticeService;
@@ -22,7 +25,10 @@ public class NoticeController {
 	}
 	
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name="p", defaultValue = "1") int page) throws ClassNotFoundException, SQLException {
+		//String p =request.getParameter("p");
+		System.out.println("page:" +page);
+		
 		
 		List<Notice> list= noticeService.getList(1, "TITLE","");
 		
